1dcefa9569771b31dc755922e034f306a6507ba4
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-08-08 10:07:51 -0300

    feat: adição do inspect e reformatação dos logs

--- utils/log.py
+++ utils/log.py
@@ -1,20 +1,39 @@
+import inspect
 from datetime import datetime
 from pathlib import Path
-import sys
 
 # define o caminho raiz do projeto
-ROOT_PATH = Path(__file__).parent.parent
-sys.path.append(str(ROOT_PATH / ''))
+UTILS_PATH = Path(__file__).parent.resolve()
+LOG_FILE = UTILS_PATH / "log.txt"
+
 
 # decorador para registrar transações no log.txt
 def transaction_log(func):
     def envelope(*args, **kwargs):
-        result = func(*args, **kwargs)
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        sig = inspect.signature(func)
+        bound = sig.bind(*args, **kwargs)
+        bound.apply_defaults()
 
-        # registra a transação no log.txt
-        with open(ROOT_PATH / "log.txt", "a", newline= "", encoding="utf-8") as archive:
-            archive.write(f"[{now}] Function {func.__name__} executed with arguments {args[-1]}, {kwargs}. "
-                          f"Return {result}\n")
+        # OrderedDict com {nome_param: valor}
+        arguments = bound.arguments
+        cpf = arguments.get("cpf", None)
+        name = arguments.get("name", None)
+        address = arguments.get("address", None)
+        value = arguments.get("value", None)
+
+        result = func(*args, **kwargs)
+
+        # Registra a transação no log
+        now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        with open(LOG_FILE, "a", encoding="utf-8") as archive:
+            archive.write(
+                f"[{now}] Function {func.__name__} executed."
+                f"\nCPF: {cpf},"
+                f"\nName: {name},"
+                f"\nAddress: {address},"
+                f"\nValue: {value},"
+                f"\nReturn: {result}\n\n"
+            )
         return result
+
     return envelope
